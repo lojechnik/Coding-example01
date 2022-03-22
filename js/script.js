3199d7d66927021723706ecec115332057befdaa
asd das <r.artyom2012@yandex.ru>
2022-03-22 13:42:29 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+window.onscroll = function showHeader(){
+    let header = document.querySelector(".header");
+    if(window.pageYOffset > 200){
+        header.classList.add('header-fixed');
+    }
+
+}
