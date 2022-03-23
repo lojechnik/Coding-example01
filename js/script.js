7cae2e3bf85ab8f987b219ac82ef095639588a7c
asd das <r.artyom2012@yandex.ru>
2022-03-24 02:07:56 +0300

    Hamburger menu, fontsize adjustment new block in the bottom, some adaptivity improvements

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
-window.onscroll = function showHeader(){
-    let header = document.querySelector(".header");
-    if(window.pageYOffset > 200){
-        header.classList.add('header-fixed');
-    }
+// window.onscroll = function showHeader(){
+//     let header = document.querySelector(".header");
+//     if(window.pageYOffset > 200){
+//         header.classList.add('header-fixed');
+//     }
 
-}
+// }
+const headermenu = document.querySelector(".header-menu__ul");
+const hamburger = document.querySelector(".header-hamburger");
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    headermenu.classList.toggle("active");
+})
+document.querySelectorAll(".header-menu__link").forEach(n=> n. addEventListener("click", ()=>{
+    hamburger.classList.remove("active");
+    headermenu.classList.remove("active");
+}))
